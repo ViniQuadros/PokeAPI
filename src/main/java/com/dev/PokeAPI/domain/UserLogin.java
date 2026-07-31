3b5990de4fbed7288f99f79bd0d3faded5f88e0d
Lucas <lucasmicaoliveira@gmail.com>
2026-07-31 10:30:25 -0300

    login rest, service, respository and connection with web db

---
+++ src/main/java/com/dev/PokeAPI/domain/UserLogin.java
@@ -0,0 +1,30 @@
+package com.dev.PokeAPI.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "tb_user")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserLogin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
