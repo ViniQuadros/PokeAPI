b1bfedd44b4ebc213439257e72c5e94415ecc2e7
Vinicius Quadros <viniciusquadrosvq@gmail.com>
2026-08-03 16:33:18 -0300

    Fixing merge conflicts

--- src/main/java/com/dev/PokeAPI/PokeApiApplication.java
+++ src/main/java/com/dev/PokeAPI/PokeApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PokeApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PokeApiApplication.class, args);
